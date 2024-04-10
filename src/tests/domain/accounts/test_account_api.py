c9350063cbd94fceaf01098b011ba419364980bf
Daniil <jhisjfiohig14@mail.ru>
2024-04-10 23:58:18 +0700

    complete success test

--- src/tests/domain/accounts/test_account_api.py
+++ src/tests/domain/accounts/test_account_api.py
@@ -11,16 +11,11 @@ class MockRepo(IRepAccount):
         return "56986558-57f9-4117-a26f-05fa0cffe8ee"
 
     async def get_all_account(self):
-        return {
-            "count": 1,
-            "items": [
-                {
-                    "uid": "56986558-57f9-4117-a26f-05fa0cffe8ee", 
-                    "email": "aaa@dot.com", 
-                    "name": "my_name"
-                },
-            ]
-        }
+        return [{
+            "uid": "56986558-57f9-4117-a26f-05fa0cffe8ee", 
+            "email": "aaa@dot.com", 
+            "name": "my_name"
+        }]
     async def get_account(self, _: str):
         return {
             "uid": "56986558-57f9-4117-a26f-05fa0cffe8ee", 
@@ -29,7 +24,7 @@ class MockRepo(IRepAccount):
         }
 
     async def delete_account(self, _: str):
-        return True
+        return {"status: OK"} if True else {"status: Doesn't exist"} 
 
     async def update_account(self, _: UUID, req: dict):
         return "UPDATE 1"
@@ -68,20 +63,53 @@ async def test_create_account__success(testclient, api_app):
         **req
     }
 
-# async def test_get_account__success(testclient, api_app):
-#     api_app.dependency_overrides[create_account_uc] = create_account_uc_override__success
-#     res = await testclient.get("/api/v1/accounts/")
-#     req = {
-#         "uid": "56986558-57f9-4117-a26f-05fa0cffe8ee",
-#         "email": "aaa@dot.com",
-#         "name": "my_name"
-#     }
-#     assert res.status_code == 200
-#     assert res.json() == {
-#         "count": len(req),
-#         "items": [req]
-#     }
+async def test_get_all_account__success(testclient, api_app):
+    api_app.dependency_overrides[create_account_uc] = create_account_uc_override__success
+    res = await testclient.get("/api/v1/accounts/")
+    req = [{
+        "uid": "56986558-57f9-4117-a26f-05fa0cffe8ee",
+        "email": "aaa@dot.com",
+        "name": "my_name"
+    }]
+    assert res.status_code == 200 
+    assert res.json() == {
+        "count": len(req),
+        "items": req
+    }
 
+async def test_get_account__success(testclient, api_app):
+    api_app.dependency_overrides[create_account_uc] = create_account_uc_override__success
+    uid = {"uid": "56986558-57f9-4117-a26f-05fa0cffe8ee"}
+    req = {"email": "aaa@dot.com", "name": "my_name"}
+    res = await testclient.get(f"/api/v1/accounts/{uid}")
+    assert res.status_code == 200
+    assert res.json() == {
+        "uid": "56986558-57f9-4117-a26f-05fa0cffe8ee",
+        **req
+    }
+
+async def test_delete_account__success(testclient, api_app):
+    api_app.dependency_overrides[create_account_uc] = create_account_uc_override__success
+    uid = {"uid": "56986558-57f9-4117-a26f-05fa0cffe8ee"}
+    res = await testclient.delete(f"/api/v1/accounts/{uid}")
+    assert res.status_code == 200
+    assert res.json() == {"status": "OK"} if res == True else {"status": "Doesn't exist"}
+
+async def test_patch_account__success(testclient, api_app):
+    api_app.dependency_overrides[create_account_uc] = create_account_uc_override__success
+    uid = {"uid": "56986558-57f9-4117-a26f-05fa0cffe8ee"}
+    req = {"email": "aaa@dot.com", "name": "my_name"}
+    res = await testclient.patch(f"/api/v1/accounts/{uid}", json=req)
+    assert res.status_code == 200
+    assert res.json() == "UPDATE 1"
+
+async def test_put_account__success(testclient, api_app):
+    api_app.dependency_overrides[create_account_uc] = create_account_uc_override__success
+    uid = {"uid": "56986558-57f9-4117-a26f-05fa0cffe8ee"}
+    req = {"email": "aaa@dot.com", "name": "my_name"}
+    res = await testclient.put(f"/api/v1/accounts/{uid}?email={req['email']}&name={req['name']}")
+    assert res.status_code == 200
+    assert res.json() == "UPDATE 1"
 
 async def test_create_account__email_busy(testclient, api_app):
     api_app.dependency_overrides[create_account_uc] = create_account_uc_override__error
