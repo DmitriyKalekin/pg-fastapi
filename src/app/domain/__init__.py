ada70f5e246683918bf21b22014f5de26f33df31
Daniil <jhisjfiohig14@mail.ru>
2024-04-11 00:01:48 +0700

    init project file

--- src/app/domain/__init__.py
+++ src/app/domain/__init__.py
@@ -1,9 +1,10 @@
 # do not remove 
 from app.domain.accounts.account_api import router as account_api
+from app.domain.projects.projects_api import router as project_api
 
 routers = [
    account_api,
-   #project_api
+   project_api
 ]
 
 
